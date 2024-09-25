71475493f7ee26e2b83719dcece56e17ebad66f5
withbyeongk <withbyeongk@gmail.com>
2024-09-26 01:49:39 +0900

    fix: 특정 유저의 포인트를 사용하는 기능 컨트롤러에서 동작하도록 수정

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -54,6 +54,6 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointService.use(id, amount);
     }
 }
